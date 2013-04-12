dfff4fb67a40768303fe989c412b949dfb688b2b
YourFullName <konilovsky@gmail.com>
2013-04-12 18:46:58 +0400

    Using external Clojure Netty tcp echo server

---
+++ src/main/java/ru/ttk/clojure/RunClojureHttpServer.java
@@ -0,0 +1,23 @@
+package ru.ttk.clojure;
+
+import clojure.lang.RT;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Date: 4/12/13
+ * Time: 6:42 PM
+ *
+ * @author konilovsky@gmail.com
+ * @since 1.0
+ */
+public class RunClojureHttpServer implements Serializable {
+
+    public static void main(String... args) throws IOException {
+        RT.loadResourceScript("go/srv.clj");
+        RT.var("go.srv", "srvr2").invoke(5000);
+        System.out.println("Server started at 5000");
+    }
+
+}
